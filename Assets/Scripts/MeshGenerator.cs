46be6141195e8aa5867ecc020af8fe5f8395a0fc
bmgaytan <bmgaytan3114@gmail.com>
2025-08-27 19:51:41 -0400

    fixed MeshGenerator.cs script

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -13,6 +13,11 @@ public class MeshGenerator : MonoBehaviour
 
     Vector3[] vertices;
     int[] triangles;     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Color[] colors;
+    public Gradient gradient;
+
+    float maxTerrainHeight;
+    float minTerrainHeight;
     void Start()
     {
         mesh = new Mesh();
@@ -35,6 +40,15 @@ public class MeshGenerator : MonoBehaviour
             {
                 float y = Mathf.PerlinNoise(x * .3f, z * .3f) * 2f;
                 vertices[i] = new Vector3(x, y, z);
+
+                if (y > maxTerrainHeight)
+                {
+                    maxTerrainHeight = y;
+                }
+                if (y < minTerrainHeight)
+                {
+                    minTerrainHeight = y;
+                }
                 i++;
             }
         }
@@ -59,6 +73,19 @@ public class MeshGenerator : MonoBehaviour
 
             vert++;
         }
+
+        colors = new Color[vertices.Length];
+        for (int i = 0, z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                float height = Mathf.InverseLerp(minTerrainHeight, maxTerrainHeight, vertices[i].y);
+                colors[i] = gradient.Evaluate(height);
+                i++;
+            }
+
+        }
+
     }
 
     // Update is called once per frame
@@ -68,10 +95,11 @@ public class MeshGenerator : MonoBehaviour
 
         mesh.vertices = vertices;
         mesh.triangles = triangles;
+        mesh.colors = colors;
 
         mesh.RecalculateNormals();
     }
-
+    
     void OnDrawGizmos()
     {
         if (vertices == null)
