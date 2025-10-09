e3a76a13b03b5b8aeea5a1608e56aacc38f03271
bmgaytan <bmgaytan3114@gmail.com>
2025-10-08 22:57:48 -0400

    Added Labels for UI, changed camera position

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -2,19 +2,25 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using System.Threading.Tasks;
+using UnityEngine.UIElements;
+using UnityEngine.InputSystem;
 public class UI : MonoBehaviour
 {
+    public MapGenerator mapGenerator;
+
     public TMP_Dropdown mapDropDown;
     public TMP_InputField mapWidth;
     public TMP_InputField mapHeight;
-    public Slider scaleSlider;
-    public Slider octaveSlider;
-    public Slider persistenceSlider;
-    public Slider lacunaritySlider;
+    public UnityEngine.UI.Slider scaleSlider;
+    public UnityEngine.UI.Slider octaveSlider;
+    public UnityEngine.UI.Slider persistenceSlider;
+    public UnityEngine.UI.Slider lacunaritySlider;
     public TMP_InputField seedField;
-    public MapGenerator mapGenerator;
 
-
+    public TMP_Text scaleLabel;
+    public TMP_Text octaveLabel;
+    public TMP_Text persistenceLabel;
+    public TMP_Text lacunarityLabel;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -48,26 +54,30 @@ public class UI : MonoBehaviour
         {
             mapGenerator.noiseScale = v;
             UpdateTerrain();
+            UpdateLabels();
         });
 
         octaveSlider.onValueChanged.AddListener((v) =>
         {
             mapGenerator.octaves = Mathf.RoundToInt(v);
             UpdateTerrain();
+            UpdateLabels();
         });
 
         persistenceSlider.onValueChanged.AddListener((v) =>
         {
             mapGenerator.persistance = v;
             UpdateTerrain();
+            UpdateLabels();
         });
 
         lacunaritySlider.onValueChanged.AddListener((v) =>
         {
             mapGenerator.lacunarity = v;
             UpdateTerrain();
+            UpdateLabels();
         });
-        
+
         seedField.onEndEdit.AddListener((text) =>
         {
             if (int.TryParse(text, out int seed))
@@ -76,12 +86,17 @@ public class UI : MonoBehaviour
                 UpdateTerrain();
             }
         });
+
+        UpdateLabels();
     }
 
     // Update is called once per frame
-    void Update()
+    private void UpdateLabels()
     {
-
+        if (scaleLabel) scaleLabel.text = $"Scale: {scaleSlider.value:F1}";
+        if (octaveLabel) octaveLabel.text = $"Octaves: {octaveSlider.value}";
+        if (persistenceLabel) persistenceLabel.text = $"Persistence: {persistenceSlider.value}";
+        if (lacunarityLabel) lacunarityLabel.text = $"Lacunarity: {lacunaritySlider.value:F1}";
     }
     private void UpdateTerrain()
     {
